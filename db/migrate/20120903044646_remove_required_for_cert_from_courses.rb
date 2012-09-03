66d96ffc01877138415618ee670eb0869f16df86
Kevin Fallon <kfallon@seneschal.com>
2012-09-03 00:51:46 -0400

    Created migration to remove required_for columns from courses table

---
+++ db/migrate/20120903044646_remove_required_for_cert_from_courses.rb
@@ -0,0 +1,13 @@
+class RemoveRequiredForCertFromCourses < ActiveRecord::Migration
+  def up
+    remove_column :courses, :required_for_pd
+    remove_column :courses, :required_for_cert
+    remove_column :courses, :required_for_sar
+  end
+  
+  def down
+    add_column :courses, :required_for_pd, :string
+    add_column :courses, :required_for_cert, :string
+    add_column :courses, :required_for_sar, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120902071807) do
+ActiveRecord::Schema.define(:version => 20120903044646) do
 
   create_table "certs", :force => true do |t|
     t.integer  "person_id"
@@ -40,11 +40,8 @@ ActiveRecord::Schema.define(:version => 20120902071807) do
     t.string   "category"
     t.integer  "duration"
     t.integer  "term"
-    t.string   "required_for_pd"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
-    t.string   "required_for_cert"
-    t.string   "required_for_sar"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "courses_skills", :id => false, :force => true do |t|
