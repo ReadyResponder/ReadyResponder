21e91eaa726c33703b02d5b0f331a9040a03d3b2
Kevin Fallon <kfallon@seneschal.com>
2012-10-23 10:45:51 -0400

    Tuning of unicorn shell script

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -3,7 +3,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/apps/blog/current
+APP_ROOT=/home/deployer/apps/lims/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=deployer
