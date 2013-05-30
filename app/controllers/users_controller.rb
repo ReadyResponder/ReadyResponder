c882272ba419c3fe17218999367add0ed6147dbe
Kevin Fallon <kfallon@seneschal.com>
2013-05-29 22:02:53 -0400

    Now have a usable UI for user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
   load_and_authorize_resource
   
-  def index
+  def xindex
     @users = User.all
 
     respond_to do |format|
