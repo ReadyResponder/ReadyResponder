b3763271c2ac4b36c6f5059b377a2aac96675515
Kevin Fallon <kfallon@seneschal.com>
2013-08-13 16:19:37 -0400

    Added mobile phone to the view by using a class method

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,10 +2,15 @@ class Channel < ActiveRecord::Base
   attr_accessible :carrier, :category, :content, :last_verified, :name, :person_id, :priority, :usage, :status
   
   belongs_to :person
+
+  def self.mobile
+   where("category LIKE ?", "%Mobile%")
+  end	
+  
   
   PRIORITIES = ['1-Call First','2','3','4-Call Last']
   USAGES = ['1-All','2-Emergency Only','3-Info Only','4-Testing']
   STATUSES = ['OK','Wrong Number','Bouncing']
-  CATEGORIES = ['Mobile Phone', 'Personal Mobile', 'Business Mobile','E-Mail','Personal E-Mail','Business E-Mail','Home Phone','Work Phone']
+  CATEGORIES = ['Mobile Phone', 'E-Mail','Personal E-Mail','Business E-Mail','Home Phone','Work Phone']
   CARRIERS = ['Sprint','Verizon','AT&T','Metro PCS','T-Mobile','Comcast','Other (Specify in Name)']
 end
