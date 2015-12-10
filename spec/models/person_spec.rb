cdea5bfd0d13a64aa1383846e82916017cd87087
Marnen Laibow-Koser <marnen@marnen.org>
2015-12-10 13:00:31 -0500

    Remove useless specs and tests. [#54]

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,25 +1,4 @@
 require 'spec_helper'
 
 describe Person do
-
-  describe "search" do
-    it "should return a person with the same firstname" do
-      FactoryGirl.create(:person, :firstname => "John")
-      FactoryGirl.create(:person, :firstname => "Jane")
-      expect(Person.search("John").count).to eq(1)
-    end
-
-    it "should return a person with the same lastname" do
-      FactoryGirl.create(:person, :lastname => "Doe")
-      FactoryGirl.create(:person, :lastname => "Stag")
-      expect(Person.search("Doe").count).to eq(1)
-    end
-
-    it "should return a person with the same id" do
-      FactoryGirl.create(:person, :icsid => "123")
-      FactoryGirl.create(:person, :icsid => "23")
-      expect(Person.search("123").count).to eq(1)
-    end
-  end
-
 end
