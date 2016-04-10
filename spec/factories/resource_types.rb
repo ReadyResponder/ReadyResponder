20965fb8a9d9878429a0239439f2e54539fe0c5f
Chris Houhoulis <chris@chrishouhoulis.com>
2016-04-10 11:43:14 -0400

    Fix indenting in FactoryGirl generated factories.

--- spec/factories/resource_types.rb
+++ spec/factories/resource_types.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :resource_type do
     name "MyString"
-status "MyString"
-description "MyText"
-fema_code "MyString"
-fema_kind "MyString"
+    status "MyString"
+    description "MyText"
+    fema_code "MyString"
+    fema_kind "MyString"
   end
 
 end
