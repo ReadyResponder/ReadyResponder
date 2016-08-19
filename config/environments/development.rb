77afb57419ff3e4a7aa93291209290d4c6ea666e
David Roth <droth@pobox.com>
2016-08-19 00:31:24 -0400

    raise exception on unpermitted parameter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,6 @@ Rails.application.configure do
   }
   config.action_mailer.default_url_options = { :host => 'localhost:3000' } # this is used by Devise for confirmation e-mails et al
   
-  # config.action_controller.action_on_unpermitted_parameters = :raise
+  config.action_controller.action_on_unpermitted_parameters = :raise
 
 end
