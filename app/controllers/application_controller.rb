003c0dfc019fb3fc135c10ebf73460de9f35b7d6
David Roth <droth@pobox.com>
2016-08-19 00:31:49 -0400

    add fields to devise parameters whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "Access Denied"
     redirect_to root_url
   end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :username
+      devise_parameter_sanitizer.for(:account_update) << :username
+    end
 end
