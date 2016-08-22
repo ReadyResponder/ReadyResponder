12ecc15786e26e9204b942536afb14d631b104bc
David Roth <droth@pobox.com>
2016-08-22 15:50:36 -0400

    turn off turbolinks (does not work with current datatables configuration)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//  require turbolinks
 //= require jquery-ui/core
 //= require jquery-ui/datepicker
 //= require jquery-ui/widget
