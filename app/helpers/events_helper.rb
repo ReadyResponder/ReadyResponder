38967c1c358f40695a479fb39af71c560e9f8eac
David Roth <droth@pobox.com>
2016-09-09 13:43:26 -0400

    fix button style in events_helper.rb

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,35 +6,35 @@ module EventsHelper
 
   def unavailable_button(timecard)
     if can? :update, timecard and ["Scheduled", "In-session"].include? @event.status
-      button = link_to("Unavailable", timecard_path(timecard, intention: "Unavailable"), method: :Put, class: 'btn btn-xs btn-default')
+      button = link_to("Unavailable", timecard_path(timecard, intention: "Unavailable"), method: :Put, class: 'btn btn-xs btn-primary')
     end
     return raw button
   end
 
   def available_button(timecard)
     if can? :update, timecard and ["Scheduled", "In-session"].include? @event.status
-      button = link_to("Available", timecard_path(timecard, intention: "Available"), method: :Put, class: 'btn btn-xs btn-default')
+      button = link_to("Available", timecard_path(timecard, intention: "Available"), method: :Put, class: 'btn btn-xs btn-primary')
     end
     return raw button
   end
 
   def scheduled_button(timecard)
     if can? :update, timecard and ["Scheduled", "In-session"].include? @event.status
-      button = link_to("Scheduled", timecard_path(timecard, intention: "Scheduled"), method: :Put, class: 'btn btn-xs btn-default')
+      button = link_to("Scheduled", timecard_path(timecard, intention: "Scheduled"), method: :Put, class: 'btn btn-xs btn-primary')
     end
     return raw button
   end
 
   def worked_button(timecard)
     if can? :update, timecard and ["In-session", "Completed"].include? @event.status
-      button = link_to("Worked", timecard_path(timecard, outcome: "Worked"), method: :Put, class: 'btn btn-xs btn-default')
+      button = link_to("Worked", timecard_path(timecard, outcome: "Worked"), method: :Put, class: 'btn btn-xs btn-primary')
     end
     return raw button
   end
 
   def edit_button(timecard)
     if can? :update, timecard
-      button = link_to('Edit', edit_timecard_path(timecard), class: "btn btn-xs btn-default")
+      button = link_to('Edit', edit_timecard_path(timecard), class: "btn btn-xs btn-primary")
     end
     return raw button
   end
