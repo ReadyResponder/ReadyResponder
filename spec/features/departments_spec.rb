83b49300ab6ef8417ad5e49fe5450fdb6ebad860
sampatbadhe <sampat.badhe@kiprosh.com>
2016-10-25 00:59:02 +0530

    cover specs for departments

---
+++ spec/features/departments_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.feature "Department" do
+  describe "user in the editor role" do
+    before do
+      somebody = create(:user)
+      r = create(:role, name: 'Editor')
+      somebody.roles << r
+      visit new_user_session_path
+      fill_in 'user_email', :with => somebody.email
+      fill_in 'user_password', :with => somebody.password
+      click_on 'Sign in'
+      @department = create(:department, name: "Technology", status: "Active")
+    end
+
+    scenario "Creates Department" do
+      visit root_path
+      click_link "Department"
+      click_link "New Department"
+      fill_in "Name", with: "Police"
+      fill_in "Shortname", with: "P"
+      fill_in "Description", with: "Police department"
+      select "Active", from: "Status"
+      click_button "Create Department"
+      expect(page).to have_content "Department was successfully created."
+      expect(current_path).to eq department_path(Department.last)
+    end
+
+    scenario "Updates Department" do
+      visit edit_department_path(@department)
+      fill_in "Name", with: "Technology"
+      fill_in "Shortname", with: "Tech"
+      click_button "Update Department"
+      expect(page).to have_content "Department was successfully updated."
+      expect(current_path).to eq department_path(@department)
+    end
+  end
+
+  describe "user in the reader role" do
+    before (:each) do
+      @department = create(:department)
+      sign_in_as('Reader')
+    end
+
+    it "cannot edit department" do
+      visit edit_department_path(@department)
+      expect(page).to have_content("Access Denied")
+    end
+
+    it "cannot create a new department" do
+      click_link "Department"
+      expect(page).not_to have_content('Create')
+      visit new_department_path
+      expect(page).to have_content("Access Denied")
+    end
+
+    it "can view a department" do
+      visit department_path(@department)
+      expect(page).to have_content(@department.name)
+    end
+  end
+end
--- spec/requests/departments_spec.rb
+++ spec/requests/departments_spec.rb
@@ -1,10 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe "Departments", :type => :request do
-  describe "GET /departments" do
-    it "works! (now write some real specs)" do
+  describe "Denies public access to Departments", type: :request do
+    before(:all) do
+      @department = create :department
+    end
+
+    it "departments#create" do
+      department_attributes = FactoryGirl.attributes_for(:department)
+
+      expect {
+        post "/departments", { department: department_attributes }
+      }.to_not change(Department, :count)
+
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "departments#delete" do
+      delete department_path(id: @department.id)
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "departments#edit" do
+      get edit_department_path(id: @department.id)
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "departments#index" do
       get departments_path
-      expect(response).to have_http_status(200)
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "departments#new" do
+      get new_department_path
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "departments#show" do
+      get department_path(id: @department.id)
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "departments#update" do
+      department_attributes = @department.attributes.except("id", "created_at", "updated_at")
+      patch department_path(id: @department.id), { department: department_attributes }
+      expect(response).to redirect_to new_user_session_path
     end
   end
 end
