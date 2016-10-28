f70d9dd4cbea3c3bb54336a986eb8d3d263e915f
Arthur Brophy <artbrophy@gmail.com>
2016-10-28 05:31:30 -0400

    created the recipient model, links to person, fixes #165

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023073142) do
+ActiveRecord::Schema.define(version: 20161028092636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -327,6 +327,16 @@ ActiveRecord::Schema.define(version: 20161023073142) do
     t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "recipients", force: :cascade do |t|
+    t.integer  "notification_id"
+    t.integer  "person_id"
+    t.text     "status"
+    t.text     "response_channel"
+    t.datetime "response_time"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "repairs", force: :cascade do |t|
---
+++ spec/factories/recipients.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :recipient do
+    notification_id 1
+    person_id 1
+    status "MyText"
+    response_channel "MyText"
+    response_time "2016-10-28 05:26:36"
+  end
+end
