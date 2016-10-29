87135f688c24af8351378ad415f30a0dc15691ee
Arthur Brophy <artbrophy@gmail.com>
2016-10-29 19:08:50 -0400

    added test for valid factory to recipient spec

--- spec/models/recipient_spec.rb
+++ spec/models/recipient_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Recipient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(create(:recipient)).to be_valid
+  end
 end
