061ae52e6e50dc07dc44935838cf703f124f52a5
Arthur Brophy <artbrophy@gmail.com>
2016-10-28 21:10:49 -0400

    optimizes tests for TimeRoundingService

--- spec/services/time_rounding_service_spec.rb
+++ spec/services/time_rounding_service_spec.rb
@@ -5,57 +5,20 @@ RSpec.describe TimeRoundingService do
   describe "#closest_fifteen" do
 
     context "given an event" do
-      it "removes milliseconds from start_time" do
-        @event = build(:event)
+
+      it "rounds times to the closest quarter hour" do
+        @event = build_stubbed(:event)
+        @event.start_time = @event.start_time.change(:hour => 9, :min => 1,
+                                                     :sec => 35, :usec => 231)
+        @event.end_time = @event.end_time.change(:hour => 9, :min => 14,
+                                                     :sec => 35, :usec => 231)
+
         @rounded_event = TimeRoundingService.new(@event).closest_fifteen
-        expect(@rounded_event.start_time.strftime("%L")).to eq("000")
+
+        expect(@rounded_event.start_time.strftime("%H:%M:%S:%L")).to eq("09:15:00:000")
+        expect(@rounded_event.end_time.strftime("%H:%M:%S:%L")).to eq("09:00:00:000")
       end
 
-      it "removes seconds from start_time" do
-        @event = build(:event)
-        @rounded_event = TimeRoundingService.new(@event).closest_fifteen
-        expect(@rounded_event.start_time.strftime("%S")).to eq("00")
-      end
-
-      it "removes milliseconds from end_time" do
-        @event = build(:event)
-        @rounded_event = TimeRoundingService.new(@event).closest_fifteen
-        expect(@rounded_event.end_time.strftime("%L")).to eq("000")
-      end
-
-      it "removes seconds from end_time" do
-        @event = build(:event)
-        @rounded_event = TimeRoundingService.new(@event).closest_fifteen
-        expect(@rounded_event.end_time.strftime("%S")).to eq("00")
-      end
-
-      it "rounds start time to closest quarter hour after given time" do
-        @event = build(:event)
-        @event.start_time = @event.start_time.change(:min => 14)
-        @rounded_event = TimeRoundingService.new(@event).closest_fifteen
-        expect(@rounded_event.start_time.strftime("%M")).to eq("15")
-      end
-
-      it "rounds start time to closest quarter hour after given time" do
-        @event = build(:event)
-        @event.start_time = @event.start_time.change(:min => 2)
-        @rounded_event = TimeRoundingService.new(@event).closest_fifteen
-        expect(@rounded_event.start_time.strftime("%M")).to eq("15")
-      end
-
-      it "rounds end time to closest quarter hour before given time" do
-        @event = build(:event)
-        @event.end_time = @event.end_time.change(:min => 14)
-        @rounded_event = TimeRoundingService.new(@event).closest_fifteen
-        expect(@rounded_event.end_time.strftime("%M")).to eq("00")
-      end
-
-      it "rounds end time to closest quarter hour before given time" do
-        @event = build(:event)
-        @event.end_time = @event.end_time.change(:min => 2)
-        @rounded_event = TimeRoundingService.new(@event).closest_fifteen
-        expect(@rounded_event.end_time.strftime("%M")).to eq("00")
-      end
     end
 
   end
