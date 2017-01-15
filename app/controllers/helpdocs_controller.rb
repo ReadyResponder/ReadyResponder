cdb4b8d024180b2bb0abe802f2027ad44d027a43
masukomi <masukomi@masukomi.org>
2017-01-15 14:14:28 -0500

    HelpdocsController#index now excludes index file

--- app/controllers/helpdocs_controller.rb
+++ app/controllers/helpdocs_controller.rb
@@ -2,8 +2,10 @@ class HelpdocsController < ApplicationController
 
   def index
     dir = Rails.root.join('app', 'views', 'helpdocs')
-    @files = Dir.entries(dir).select{|f| f.match(/^\w.*\.md*/)}
-      .map{|f| f.gsub(/\..*/, '')}.sort
+    @files = (
+      (Dir.entries(dir).select{|f| f.match(/^\w.*\.md*/)}
+      .map{|f| f.gsub(/\..*/, '')}) - ['index']
+    ).sort
     # todo eventually use `tree` or something to display a
     # tree of available helpdocs. BUT #show doesn't currently
     # support a tree of them yet.
