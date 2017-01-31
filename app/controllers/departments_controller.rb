fa85423a66c1fb736ffbcbf2768221463e4f5325
Kevin Fallon <kfallon@rbmtechnologies.com>
2017-01-30 21:54:55 -0500

    Improving grade of departments controller, and tweak to show page

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,103 +1,63 @@
 class DepartmentsController < ApplicationController
   before_filter :authenticate_user!
   before_action :format_divisions, :only => [:create, :update]
+  before_action :set_department, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
 
-  # GET /departments
-  # GET /departments.json
   def index
     @departments = Department.all
     @page_title = "All Departments"
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @departments }
-    end
   end
 
-  # GET /departments/1
-  # GET /departments/1.json
   def show
-    @department = Department.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @department }
-    end
   end
 
-  # GET /departments/new
-  # GET /departments/new.json
   def new
     @department = Department.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @department }
-    end
+    @page_title = "New Department"
   end
 
-  # GET /departments/1/edit
   def edit
-    @department = Department.find(params[:id])
   end
 
-  # POST /departments
-  # POST /departments.json
   def create
     @department = Department.new(department_params)
-
-    respond_to do |format|
-      if @department.save
-        format.html { redirect_to @department, notice: 'Department was successfully created.' }
-        format.json { render json: @department, status: :created, location: @department }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @department.errors, status: :unprocessable_entity }
-      end
+    if @department.save
+      redirect_to @department, notice: 'Department successfully created.'
+    else
+      render action: "new"
     end
   end
 
-  # PATCH/PUT /departments/1
-  # PATCH/PUT /departments/1.json
   def update
-    @department = Department.find(params[:id])
-    respond_to do |format|
-      if @department.update_attributes(department_params)
-        format.html { redirect_to @department, notice: 'Department was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @department.errors, status: :unprocessable_entity }
-      end
+    if @department.update_attributes(department_params)
+      redirect_to @department, notice: 'Department successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
-  # DELETE /departments/1
-  # DELETE /departments/1.json
   def destroy
-    @department = Department.find(params[:id])
     @department.destroy
-
-    respond_to do |format|
-      format.html { redirect_to departments_url }
-      format.json { head :no_content }
-    end
+    redirect_to departments_url, notice: 'Department successfully destroyed.'
   end
 
   private
 
-    # Takes the passed in CSV and converts it into an array that removes any leading/trailing whitespace on each item
-    def format_divisions
-      params[:department][:division1] = params[:department][:division1].split(',').map{|x| x.chomp.lstrip}
-      params[:department][:division2] = params[:department][:division2].split(',').map{|x| x.chomp.lstrip}
-    end
+  # Takes the passed in CSV and converts it into an array that removes any leading/trailing whitespace on each item
+  def format_divisions
+    params[:department][:division1] = params[:department][:division1].split(',').map{|x| x.chomp.lstrip}
+    params[:department][:division2] = params[:department][:division2].split(',').map{|x| x.chomp.lstrip}
+  end
 
-    # Use this method to whitelist the permissible parameters. Example:
-    # params.require(:person).permit(:name, :age)
-    # Also, you can specialize this method with per-user checking of permissible attributes.
-    def department_params
-      params.require(:department).permit(:name, :shortname, :status,
-        :contact_id, :description, :manage_items, :manage_people, :division1 => [], :division2 => [])
-    end
+  def set_department
+    @department = Department.find(params[:id])
+    @page_title = "Dept: #{@department.name}"
+  end
+
+  def department_params
+    params.require(:department).permit(:name, :shortname, :status,
+      :contact_id, :description, :manage_items, :manage_people,
+      :division1 => [], :division2 => [])
+  end
 end
