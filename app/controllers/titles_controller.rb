d59d40d099c78453c6c3bc7064f12fecdb6bb7f7
Van Auken <kvanauken@seneschal.com>
2017-02-24 12:41:50 -0500

    362 Remove references to json and restating html format

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -1,85 +1,48 @@
 class TitlesController < ApplicationController
   before_filter :authenticate_user!
   load_and_authorize_resource
-  # GET /titles
-  # GET /titles.json
+
   def index
     @titles = Title.all
     @page_title = "All Titles"
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @titles }
-    end
   end
 
-  # GET /titles/1
-  # GET /titles/1.json
   def show
     @title = Title.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @title }
-    end
   end
 
-  # GET /titles/new
-  # GET /titles/new.json
   def new
     @title = Title.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @title }
-    end
   end
 
-  # GET /titles/1/edit
   def edit
     @title = Title.find(params[:id])
   end
 
-  # POST /titles
-  # POST /titles.json
   def create
     @title = Title.new(params[:title])
 
-    respond_to do |format|
-      if @title.save
-        format.html { redirect_to @title, notice: 'Title was successfully created.' }
-        format.json { render json: @title, status: :created, location: @title }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @title.errors, status: :unprocessable_entity }
-      end
+    if @title.save
+      redirect_to @title, notice: 'Title was successfully created.'
+    else
+      render action: "new"
     end
   end
 
-  # PUT /titles/1
-  # PUT /titles/1.json
   def update
     @title = Title.find(params[:id])
 
-    respond_to do |format|
-      if @title.update_attributes(params[:title])
-        format.html { redirect_to @title, notice: 'Title was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @title.errors, status: :unprocessable_entity }
-      end
+    if @title.update_attributes(params[:title])
+      redirect_to @title, notice: 'Title was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
-  # DELETE /titles/1
-  # DELETE /titles/1.json
   def destroy
     @title = Title.find(params[:id])
     @title.destroy
 
-    respond_to do |format|
-      format.html { redirect_to titles_url }
-      format.json { head :no_content }
-    end
+    redirect_to titles_url
   end
 end
