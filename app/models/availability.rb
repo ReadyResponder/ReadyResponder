e57ae822a7e04120201218c26d02e611d3b7708a
Arthur Brophy <artbrophy@gmail.com>
2017-03-17 02:57:43 -0400

    cancel_duplicate method doesn't change status of self

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -48,7 +48,7 @@ class Availability < ActiveRecord::Base
     previous_availabilities = person.availabilities.for_time_span(start_time..end_time).order(:created_at)
     previous_availabilities.each do |a|
       if start_time == a.start_time && end_time == a.end_time
-        a.update(status: "Cancelled")
+        a.update(status: "Cancelled") unless self == a
       end
     end
   end
