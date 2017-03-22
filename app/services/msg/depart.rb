a8e4575d9783fdead3ca4ab858f50b9c94b12ee9
Peter Cai <peter@privy.com>
2017-03-21 20:51:42 -0400

    use a more conventional if/else structure

--- app/services/msg/depart.rb
+++ app/services/msg/depart.rb
@@ -6,23 +6,21 @@ class Msg::Depart < Msg::Base
     Timecard.transaction do
       incomplete_timecards = @person.timecards.where(status: "Incomplete")
       
-      # If the number of incomplete timecards is not exactly 1, create an error 
-      # entry so that they can be manually reconciled
       timecards_count = @person.timecards.where(status: "Incomplete").count
-      if timecards_count != 1
-        # set all incomplete timecards to error state
+      if timecards_count == 1
+        timecard = incomplete_timecards.first
+        timecard.update!(status: "Unverified", end_time: Time.now)
+        return "Timecard completed."
+      else
+        # If the number of incomplete timecards is not exactly 1:
+        # set all incomplete timecards to error state...
         incomplete_timecards.update_all(status: "Error")
-        # record the depart time as a new timecard entry
+        # ...then record the depart time as a new timecard entry.
         Timecard.create!(person_id: @person.id,
                          status: "Error",
                          end_time: Time.now)
         return "Error: #{timecards_count} incomplete timecards."
       end
-      
-      timecard = incomplete_timecards.first
-      timecard.update!(status: "Unverified", end_time: Time.now)
-      
-      return "Timecard completed."
     end
   end
 end
