40688d38801a5a65baff95d09a8bda48f0518c3c
Kevin Fallon <kfallon@rbmtechnologies.com>
2017-09-14 16:50:07 -0400

    Now showing colors in the view.
    Sadly, they are hard coded currently. Need to move the ranges into the
    department record.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,9 +1,24 @@
 module PeopleHelper
-  def display_date_with_days_hint(a_date)
-    html = "<td title=#{a_date} >"
-    html += distance_of_time_in_words(a_date, Date.today)
+  def display_date_with_days_hint(person, field_name)
+    html = '<td title="'
+    html += person.send(field_name).strftime('%a %b %d') + '" '
+    html += td_class_string(person, field_name) + ' > '
+    html += distance_of_time_in_words(person.send(field_name), Date.today)
     html += '</td>'
 
     raw html
+  # return 1/0
+  end
+
+  private
+  def td_class_string(person, field_name)
+    case (Date.today - person.send(field_name).to_date).to_i
+    when 14..60000
+      return 'class="danger"'
+    when 7..14
+      return 'class="warning"'
+    else
+      return ''
+    end
   end
 end
