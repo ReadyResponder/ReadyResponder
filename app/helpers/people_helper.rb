634fc7cea2f619de0b97417bc3cf8d46bc54ebd1
Kevin Fallon <kfallon@rbmtechnologies.com>
2017-09-14 15:02:26 -0400

    Now using new person method's in the view
    This will show how recently they have been active

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,9 @@
 module PeopleHelper
+  def display_date_with_days_hint(a_date)
+    html = "<td title=#{a_date} >"
+    html += distance_of_time_in_words(a_date, Date.today)
+    html += '</td>'
+
+    raw html
+  end
 end
