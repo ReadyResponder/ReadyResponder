bdc00a72428a7d91653d7ba45974316b8a24b221
danielstrimpel <dastrimpel@gmail.com>
2017-09-18 10:37:42 -0400

    Added cancelled status option to timecard

--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -6,7 +6,7 @@ class Timecard < ActiveRecord::Base
 
   belongs_to :person
 
-  STATUS_CHOICES = ['Incomplete', 'Unverified', "Error", "Verified"]
+  STATUS_CHOICES = ['Incomplete', 'Unverified', "Error", "Verified", "Cancelled"]
 
   validates_presence_of :person_id
   validates_chronology :start_time, :end_time
