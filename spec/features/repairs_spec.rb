f84aef13790853af24c1d097e3ba010cd486398e
Ryan Angelo <cadagryanangelo@gmail.com>
2017-10-08 20:28:26 +0800

    test /item_categories to check table of items need to repair

--- spec/features/repairs_spec.rb
+++ spec/features/repairs_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Repairs" do
+  let!(:item) { create(:item) }
+  let!(:person) { create(:person) }
+  let!(:repair) { create(:repair) }
   before(:each) { sign_in_as('Editor') }
 
   describe "GET /repairs" do
@@ -8,6 +11,14 @@ RSpec.describe "Repairs" do
       visit repairs_path
       expect(page).to have_content("Listing Repairs")
       expect(page).to have_content("Home") # This is in the nav bar
+    end
+  end
+
+  describe "GET /item_categories" do
+    it "will display items need to repair" do
+      visit item_categories_path
+      expect(page).to have_content "Repairs"
+      expect(page).to have_css "table"
     end
   end
 end
