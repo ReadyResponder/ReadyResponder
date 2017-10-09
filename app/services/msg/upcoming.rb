92cad8c00748dea5cbc35b36000f36a022e116b8
Sandeep Acharya <acharya877@gmail.com>
2017-10-09 06:56:42 +0545

    refactor upcoming events response to remove excess if filter

--- app/services/msg/upcoming.rb
+++ app/services/msg/upcoming.rb
@@ -3,28 +3,26 @@ class Msg::Upcoming < Msg::Base
   def respond
     events = Event.upcoming.limit(10)
     response = ""
-    events.each do |event|
-      if event.departments.include?(@person.department)
-        if event.people.include?(@person)
-          response += "🔷 "
-        elsif event.available_people.include?(@person)
-          response += "✅ "
-        elsif event.partially_available_people.include?(@person)
-          response += "⚠️ "
-        elsif event.unavailable_people.include?(@person)
-          response += "❌ "
-        else
-          response += "? "
-        end
-        response += "#{event.start_time.strftime('%a %b %d %k:%M')} - " if event.start_time.present?
-        response += "#{event.end_time.strftime('%b %d %k:%M')} " if event.end_time.present?
-        response += "[#{event.id_code.to_s}] " if event.id_code.present?
-        response += event.title.truncate(23) if event.title.present?
-        # TODO need to show their status for this event (A, U, X)
-        # TODO need to check if they have already responded
-        # TODO need to check if they have already assigned to a task
-        response += "\n"
+    @person.department.events.each do |event|
+      if event.people.include?(@person)
+        response += "🔷 "
+      elsif event.available_people.include?(@person)
+        response += "✅ "
+      elsif event.partially_available_people.include?(@person)
+        response += "⚠️ "
+      elsif event.unavailable_people.include?(@person)
+        response += "❌ "
+      else
+        response += "? "
       end
+      response += "#{event.start_time.strftime('%a %b %d %k:%M')} - " if event.start_time.present?
+      response += "#{event.end_time.strftime('%b %d %k:%M')} " if event.end_time.present?
+      response += "[#{event.id_code.to_s}] " if event.id_code.present?
+      response += event.title.truncate(23) if event.title.present?
+      # TODO need to show their status for this event (A, U, X)
+      # TODO need to check if they have already responded
+      # TODO need to check if they have already assigned to a task
+      response += "\n"
     end
     return response
   end
