2b62946054d0bcfddd2d916f712ecf3cfbc5ebfd
goldenhub777 <sg300c@gmail.com>
2017-10-09 23:21:13 -0400

    removed unused whitespace in event_spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -48,8 +48,6 @@ RSpec.describe Event do
     expect(@event.id_code).to eq("howdy")
   end
 
-
-
   context "finds the correct events as concurrent" do
     it "doesn't find an old event" do
       @old_event = create(:event, id_code: "old", start_time: Time.at(111), end_time: Time.at(222))
