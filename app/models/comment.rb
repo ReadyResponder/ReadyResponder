9c1b1d6f8fd6e344fe77a98f833522f1c18f75e8
Alex Shook <alexandra.shook@gmail.com>
2017-10-10 22:21:59 -0400

    Add Comment validations for person_id, description
    Also updates comment factory.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
 
   scope :recent, ->{ order('created_at DESC') }
+
+  validates_presence_of :person_id, :description
 end
