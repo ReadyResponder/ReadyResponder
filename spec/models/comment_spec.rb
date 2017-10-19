6e34106dafee128966a3de0770df6fc690a61e04
Alex Shook <alexandra.shook@gmail.com>
2017-10-18 23:14:10 -0400

    Also remove the test for the person_id validation

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Comment do
   describe 'validations' do
-    it { is_expected.to validate_presence_of :person_id }
     it { is_expected.to validate_presence_of :description }
   end
 
