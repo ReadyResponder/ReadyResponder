461ab4ffba4208f2b2ea4e36e82ede82e8b89110
goldenhub777 <sg300c@gmail.com>
2017-10-18 21:54:55 -0400

    fixed event_controller and events_spec tests

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @events = all_events_except_templates
+    @events = Event.recent
     @page_title = "Events"
   end
 
@@ -13,7 +13,7 @@ class EventsController < ApplicationController
   end
 
   def archives
-    @archives = all_events_except_templates
+    @archives = Event.actual
     @page_title = "Archives"
   end
 
@@ -57,10 +57,6 @@ class EventsController < ApplicationController
   end
 
   private
-
-  def all_events_except_templates
-    Event.where(is_template: false)
-  end
 
   def event_params
     params.require(:event).permit(:title, :description, :category,
