da84c0be36405b81e68984092791d685009fd5e1
Nuno Esteves <nesteves@users.noreply.github.com>
2017-11-02 23:54:34 +0000

    Refactor the feature tests in events for filtering, creation and updating.

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -1,5 +1,172 @@
 require 'rails_helper'
 
+RSpec.feature 'Events filtering', js: true do
+  context 'logged in as an Editor' do
+    before(:each) do
+      sign_in_as('Editor')
+    end
+
+    scenario 'given a set of events, I can search for one of them by its title' do
+      event = create(:event, title: 'The event')
+      other_events = create_list(:event, 2)
+      event_name = event.title
+
+      visit events_path
+      fill_in 'Search', with: event_name
+
+      within 'table#events tbody' do
+        expect(page).to have_content(event_name)
+        expect(page).not_to have_content(other_events.first.title)
+        expect(page).not_to have_content(other_events.second.title)
+      end
+    end
+
+    scenario 'given a set of templates, I can search for one of them by its title' do
+      template = create(:event, is_template: true, title: 'The template event')
+      other_templates = create_list(:event, 2, is_template: true)
+      template_name = template.title
+
+      visit events_templates_path
+      fill_in 'Search', with: template_name
+
+      within 'table#templates tbody' do
+        expect(page).to have_content(template_name)
+        expect(page).not_to have_content(other_templates.first.title)
+        expect(page).not_to have_content(other_templates.second.title)
+      end
+    end
+
+    scenario 'given a set of archived events, I can search for one of them by its title' do
+      archived_event = create(:event, status: 'Completed', title: 'The template event')
+      other_archived_events = create_list(:event, 2, status: 'Completed')
+      archived_event_name = archived_event.title
+
+      visit events_archives_path
+      fill_in 'Search', with: archived_event_name
+      within 'table#archives tbody' do
+        expect(page).to have_content(archived_event_name)
+        expect(page).not_to have_content(other_archived_events.first.title)
+        expect(page).not_to have_content(other_archived_events.second.title)
+      end
+    end
+
+    scenario 'given a set of events, if I search for a title that does not exists, I get no results' do
+      events = create_list(:event, 3)
+
+      visit events_path
+      fill_in 'Search', with: 'A non existent title'
+
+      within 'table#events tbody' do
+        expect(page).to have_content("No matching records found")
+        expect(page).not_to have_content(events.first.title)
+        expect(page).not_to have_content(events.second.title)
+        expect(page).not_to have_content(events.third.title)
+      end
+    end
+
+    context 'given a set of past, recent and current events' do
+      before(:each) do
+        @past_event     = create(:event, start_time: 14.months.ago, end_time: 1.hour.ago, status: 'Completed')
+        @recent_event   = create(:event, start_time: 6.months.ago, end_time: 1.hour.ago, status: 'Completed')
+        @current_event  = create(:event, status: 'In-session')
+        @template_event = create(:event, is_template: true)
+      end
+
+      scenario 'I can filter the set by current and/or recent events' do
+        visit events_path
+
+        check   'js-events-current-checkbox'
+        uncheck 'js-events-recent-checkbox'
+
+        within(".dataTables_scroll") do
+          expect(page).to have_content @current_event.title
+          expect(page).not_to have_content @recent_event.title
+          expect(page).not_to have_content @past_event.title
+          expect(page).not_to have_content @template_event.title
+        end
+
+        uncheck 'js-events-current-checkbox'
+        check   'js-events-recent-checkbox'
+
+        within(".dataTables_scroll") do
+          expect(page).to have_content @current_event.title
+          expect(page).to have_content @recent_event.title
+          expect(page).not_to have_content @past_event.title
+          expect(page).not_to have_content @template_event.title
+        end
+
+        check 'js-events-current-checkbox'
+        check 'js-events-recent-checkbox'
+
+        within(".dataTables_scroll") do
+          expect(page).to have_content @current_event.title
+          expect(page).to have_content @recent_event.title
+          expect(page).not_to have_content @past_event.title
+          expect(page).not_to have_content @template_event.title
+        end 
+
+        uncheck 'js-events-current-checkbox'
+        uncheck 'js-events-recent-checkbox'
+
+        within(".dataTables_scroll") do
+          expect(page).to have_content 'No matching records found'
+        end
+      end
+    end
+  end
+end
+
+RSpec.feature 'Event creation and edition', js: true do
+  context 'logged in as an Editor' do
+    before(:each) do
+      sign_in_as('Editor')
+    end
+
+    scenario 'I can click on the "+" link to access a form where I can create a new event' do
+      visit events_path
+      
+      within 'table caption' do
+        click_link '+'
+      end
+      
+      fill_in 'Title', with: 'Some event'
+      select 'Meeting', from: 'event_category'
+      select 'Completed', from: 'event_status'
+      fill_in 'Description', with: 'Really Long Text...'
+      fill_in 'Start Time', with: 2.hours.from_now.strftime('%Y-%m-%d %rH:%M')
+      fill_in 'End Time', with: 4.hours.from_now.strftime('%Y-%m-%d %rH:%M')
+      # This moves the cursor out from the Time selector, clearing the popup
+      # so selenium can then click on create
+      fill_in 'Id code', with: 'event03' 
+      click_on 'Create'
+
+      expect(page).to have_content 'Event was successfully created'
+      expect(page).to have_content 'Some event'
+      expect(page).to have_content 'Code: event03'
+    end
+
+    scenario 'I can select an event to edit and then change one or more of its attributes' do
+      event = create(:event, status: 'In-session', start_time: 1.hour.from_now, end_time: 3.hours.from_now)
+
+      visit events_path
+      within 'table#events tbody tr' do
+        click_link 'Edit'
+      end
+
+      within 'form' do
+        expect(page).to have_field('Title', with: event.title)
+        expect(page).to have_select('Status', selected: event.status)
+      end
+
+      fill_in 'Title', with: 'A new smashing title!'
+      click_on 'Update'
+
+      expect(page).to have_content 'Event was successfully updated'
+      expect(page).to have_content 'A new smashing title!'
+    end
+  end
+end
+
 RSpec.describe "Events" do
   before(:each) do
     sign_in_as('Editor')
@@ -11,64 +178,6 @@ RSpec.describe "Events" do
 
   # removed sidebar so disabling this test for now
   # get_basic_editor_views('event',['Training', 'Status'])
-
-  describe "search" do
-    it "finds current event", js: true do
-      @event_two = create(:event, title: "another event")
-      @event_three = create(:event, title: "different event")
-      visit events_path
-      fill_in "Search", with: "current title"
-      within_table("events") do
-      	within("tbody") do
-      	  expect(page).to have_content(@current.title)
-          expect(page).not_to have_content(@event_two.title)
-          expect(page).not_to have_content(@event_three.title)
-      	end
-      end
-    end
-
-    it "finds template event", js: true do
-      @event_two = create(:event, title: "another event", is_template: true)
-      @event_three = create(:event, title: "different event", is_template: true)
-      visit events_templates_path
-      fill_in "Search", with: "template title"
-      within_table("templates") do
-      	within("tbody") do
-      	  expect(page).to have_content(@template.title)
-          expect(page).not_to have_content(@event_two.title)
-          expect(page).not_to have_content(@event_three.title)
-      	end
-      end
-    end
-
-    it "finds archive event", js: true do
-      @event_two = create(:event, title: "another event")
-      @event_three = create(:event, title: "different event")
-      visit events_archives_path
-      fill_in "Search", with: "archive title"
-      within_table("archives") do
-      	within("tbody") do
-      	  expect(page).to have_content(@archive.title)
-          expect(page).not_to have_content(@event_two.title)
-          expect(page).not_to have_content(@event_three.title)
-      	end
-      end
-    end
-
-    it "finds no results", js: true do
-      visit events_archives_path
-      fill_in "Search", with: "this will not find anything"
-      within_table("archives") do
-      	within("tbody") do
-      	  expect(page).not_to have_content(@template.title)
-          expect(page).not_to have_content(@current.title)
-          expect(page).not_to have_content(@recent.title)
-          expect(page).not_to have_content(@archive.title)
-          expect(page).to have_content("No matching records found")
-      	end
-      end
-    end
-  end
 
   describe "creates" do
     it "events", js: true do
@@ -221,14 +330,6 @@ RSpec.describe "Events" do
           expect(page).to have_content(@completed.title)
           expect(page).not_to have_content(@template.title)
       	end
-      end
-    end
-
-    it "an edit form" do
-      @event = create(:event, :meeting, title: "Something divine")
-      visit edit_event_path(@event)
-      within("#sidebar") do
-        expect(page).to have_content("Cancel")
       end
     end
 
