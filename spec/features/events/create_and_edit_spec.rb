25982ad20e4b9b2903e39fa7868a12b2034ea077
Nuno Esteves <nesteves@users.noreply.github.com>
2017-11-06 22:43:37 +0000

    Refactor event feature specs into separate files to match the tested feature. Add specs for creating and editing events.

---
+++ spec/features/events/create_and_edit_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.feature 'Event creation and edition', js: true do
+  context 'logged in as an Editor' do
+    before(:each) do
+      sign_in_as('Editor')
+    end
+
+    scenario 'I can click on the "+" link to access a form where I can create a new event' do
+      visit events_path
+      
+      within 'table caption' do
+        click_link '+'
+      end
+      
+      fill_in 'Title', with: 'Some event'
+      select 'Meeting', from: 'event_category'
+      select 'Completed', from: 'event_status'
+      fill_in 'Description', with: 'Really Long Text...'
+      fill_in 'Start Time', with: 2.hours.from_now.strftime('%Y-%m-%d %rH:%M')
+      fill_in 'End Time', with: 4.hours.from_now.strftime('%Y-%m-%d %rH:%M')
+      # This moves the cursor out from the Time selector, clearing the popup
+      # so selenium can then click on create
+      fill_in 'Id code', with: 'event03' 
+      click_on 'Create'
+
+      expect(page).to have_content 'Event was successfully created'
+      expect(page).to have_content 'Some event'
+      expect(page).to have_content 'Code: event03'
+    end
+
+    scenario 'I can select an event to edit and then change one or more of its attributes' do
+      event = create(:event, status: 'In-session', start_time: 1.hour.from_now, end_time: 3.hours.from_now)
+
+      visit events_path
+      within 'table#events tbody tr' do
+        click_link 'Edit'
+      end
+
+      within 'form' do
+        expect(page).to have_field('Title', with: event.title)
+        expect(page).to have_select('Status', selected: event.status)
+      end
+
+      fill_in 'Title', with: 'A new smashing title!'
+      click_on 'Update'
+
+      expect(page).to have_content 'Event was successfully updated'
+      expect(page).to have_content 'A new smashing title!'
+    end
+  end
+end
+
