6a5a63a831ff4a60813e9cd93d9edb9f0fde2fe7
Nuno Esteves <nesteves@users.noreply.github.com>
2017-11-06 23:31:45 +0000

    First draft for specs on the event view.

---
+++ spec/features/events/view_spec.rb
@@ -0,0 +1,132 @@
+require 'rails_helper'
+
+RSpec.feature 'Event page' do
+  context 'logged in as an Editor and given an event with departments and rank' do
+    before(:each) do
+      sign_in_as('Editor')
+    end
+
+    context 'given an event with a couple of departments and minimum rank' do
+      before(:each) do
+        @departments = create_list(:department, 2)
+        @event = create(:event, title: 'The Event', id_code: 'event0132',
+                        start_time: 1.day.ago, end_time: 1.day.from_now,
+                        departments: @departments)
+      end
+
+      scenario 'can be accessed from the events list' do
+        visit events_path
+
+        within('table#events tbody') do
+          expect(page).to have_content @event.title
+        end
+
+        click_link @event.title
+
+        expect(page).to have_content @event.title
+      end
+
+      scenario 'shows basic event information' do
+        visit event_path(@event)
+
+        expect(page).to have_content @event.title
+        expect(page).to have_content @event.id_code
+        expect(page).to have_content @departments.map(&:name)
+        expect(page).to have_content @event.min_title
+        expect(page).to have_content @event.description
+        expect(page).to have_content @event.status
+      end
+    end
+
+    context 'given an event with associated departments where members have differing availabilities' do
+      before(:each) do
+        Timecop.freeze
+
+        # Set up the event with multiple departments
+        @response_team   = create(:department, name: 'Response Team Dept.')
+        @medical_reserve = create(:department, name: 'Medical Reserve Dept.')
+        @event = create(:event, departments: [@response_team, @medical_reserve],
+                        start_time: 1.day.from_now, end_time: 3.days.from_now,
+                        min_title: Person::TITLE_ORDER.keys.first)
+      end
+
+      after(:each) do
+        Timecop.return
+      end
+
+      scenario 'shows assigned people by department' do
+        task  = create(:task, event: @event)
+        title = create(:title)
+        requirement = create(:requirement, task: task, title: title)
+
+        assigned_responder = create(:person, department: @response_team)
+        create(:assignment, requirement: requirement, person: assigned_responder)
+        create(:availability, person: assigned_responder, status: 'Available',
+               start_time: @event.start_time, end_time: @event.end_time)
+
+        visit event_path(@event)
+
+        expect(page).to have_css('.event-labels[title="Assigned to THIS Event"]', text: '1')
+        expect(page).to have_css('.event-labels[title="Available"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Partially Available"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Unavailable"]', text: '0')
+        expect(page).to have_css('.event-labels[title="No Response"]', text: '0')
+      end
+
+      scenario 'shows available people by department' do
+        available_doc = create(:person, department: @medical_reserve)
+        create(:availability, person: available_doc, status: 'Available',
+               start_time: @event.start_time, end_time: @event.end_time)
+
+        visit event_path(@event)
+
+        expect(page).to have_css('.event-labels[title="Assigned to THIS Event"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Available"]', text: '1')
+        expect(page).to have_css('.event-labels[title="Partially Available"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Unavailable"]', text: '0')
+        expect(page).to have_css('.event-labels[title="No Response"]', text: '0')
+      end
+
+      scenario 'shows partially available people by department' do
+        partially_av_responder = create(:person, department: @response_team)
+        create(:availability, person: partially_av_responder, status: 'Available',
+               start_time: @event.start_time + 1.day, end_time: @event.end_time + 1.day)
+
+        visit event_path(@event)
+
+        expect(page).to have_css('.event-labels[title="Assigned to THIS Event"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Available"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Partially Available"]', text: '1')
+        expect(page).to have_css('.event-labels[title="Unavailable"]', text: '0')
+        expect(page).to have_css('.event-labels[title="No Response"]', text: '0')
+      end
+
+      scenario 'shows unavailable people by department' do
+        unavailable_doc = create(:person, department: @medical_reserve)
+        create(:availability, person: unavailable_doc, status: 'Unavailable',
+               start_time: @event.start_time, end_time: @event.end_time)
+
+        visit event_path(@event)
+
+        expect(page).to have_css('.event-labels[title="Assigned to THIS Event"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Available"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Partially Available"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Unavailable"]', text: '1')
+        expect(page).to have_css('.event-labels[title="No Response"]', text: '0')
+      end
+
+      scenario 'shows people that did not respond by department' do
+        unresponsive_responder = create(:person, department: @response_team,
+                                        title_order: 30)
+
+        visit event_path(@event)
+
+        expect(page).to have_css('.event-labels[title="Assigned to THIS Event"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Available"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Partially Available"]', text: '0')
+        expect(page).to have_css('.event-labels[title="Unavailable"]', text: '0')
+        expect(page).to have_css('.event-labels[title="No Response"]', text: '1')
+      end
+    end
+  end
+end
