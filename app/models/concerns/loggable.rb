680698b0ef9678c86da4d99ec6aa273b5f60ee69
Lena Plaksina <lena.plaksina@gmail.com>
2017-11-26 14:24:50 +1300

    Integrate Loggable with paper_trail

--- app/models/concerns/loggable.rb
+++ app/models/concerns/loggable.rb
@@ -4,17 +4,9 @@ module Loggable
   included do
     after_update :log_changes_if_updated
 
-    def self.modified_by=(user)
-      @modified_by ||= user
-    end
-
-    def self.modified_by
-      @modified_by.presence
-    end
-
     private
       def log_changes_if_updated
-        log_changes if previous_changes.any?
+        log_changes if changes.any?
       end
 
       def log_changes
@@ -25,17 +17,26 @@ module Loggable
         )
       end
 
+      def modified_by
+        if paper_trail.try(:originator)
+          User.find(paper_trail.try(:originator))
+        else
+          raise "Loggable module must be used in conjunction with Paper Trail"
+        end
+      end
+
       # Can be overriden in the class for a more specific message
       def message
         "Updated #{self.class.name} #{id}, #{changes_details}"
       end
 
       def changes_details
-        previous_changes.delete("updated_at")
+        dup = changes
+        dup.delete("updated_at")
 
-        [].tap do |changes|
-          previous_changes.each do |attr_name, arr|
-            changes << "#{attr_name} changed from '#{arr[0]}' to '#{arr[1]}'"
+        [].tap do |details|
+          dup.each do |attr_name, arr|
+            details << "#{attr_name} changed from '#{arr[0]}' to '#{arr[1]}'"
           end
         end.join(", ")
       end
