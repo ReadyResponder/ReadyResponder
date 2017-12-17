aa81e19e64eb17e99e76fad828d0a74c72296abb
yasirazgar <yasirazgar@gmail.com>
2017-12-17 18:24:55 +0530

    adding setting for upcoming events counts

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -13,4 +13,22 @@ class Setting < ActiveRecord::Base
     value_object = Setting.where(key: key, status: "Active").first
     value_object.value if value_object
   end
+
+  def active?
+    self.status == 'Active'
+  end
+
+  def self.find_or_create_upcoming_events_setting(options = {})
+    find_or_create_with_defaults('UPCOMING_EVENTS_COUNT', {
+      :name => 'Upcoming events count',
+      :value => 10,
+      :category => 'Person',
+      :status => 'Active',
+      }.merge(options))
+  end
+
+  def self.find_or_create_with_defaults(key, options = {})
+    create_with(options).find_or_create_by!(:key => key)
+  end
+  
 end
--- spec/features/settings_spec.rb
+++ spec/features/settings_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe "Settings" do
 
     get_basic_editor_views('setting',['name', 'key', 'value'])
   end
+
+  describe "active?" do
+    it 'returns based on status' do
+      expect(Setting.new(:status => 'Active').active?).to be_truthy
+      expect(Setting.new(:status => 'Inactive').active?).not_to be_truthy
+    end
+  end
+
+  describe "find_or_create_upcoming_events_setting" do
+    it 'creates upcomins events setting with defaults' do      
+      setting = Setting.find_or_create_upcoming_events_setting(:value => 5)
+
+      expect(setting.status).to eq 'Active'
+      expect(setting.value).to eq '5'
+      expect(setting.category).to eq 'Person'
+      expect(setting.name).to eq 'Upcoming events count'
+    end
+  end
 end
